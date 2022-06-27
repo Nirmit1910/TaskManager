9a16d6cb3eb44262dc8d04943c6050682632bbe3
Nirmit Gupta <65971697+Nirmit1910@users.noreply.github.com>
2022-06-27 20:07:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,36 +2,31 @@ const express = require('express');
 const app=express();
 const tasks=require('./routes/tasks');
 const connectDB=require('./db/connect');
+const notFound=require('./middleware/not Found');
+const errorHandler=require('./middleware/error-handler');
 
 const dotenv = require('dotenv')
 dotenv.config({path:__dirname+'/.env'});
 
 app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api/v1/tasks',tasks);
+app.use(notFound);
+app.use(errorHandler);
+
+const port=process.env.PORT||3000;
 
 const start=async()=>{
     try{
         await connectDB(process.env.MONGO_URL);
         console.log('connected to db');
+        app.use('/api/tasks',tasks);
+        app.listen(port,()=>{
+        console.log('server started at port 3000')});
     }catch(err){
         console.log(err);
     }
-    app.use('/api/tasks',tasks);
-    app.listen(3000,()=>{
-        console.log('server started at port 5000');
-    }
-    );
 }
 start();
 
-
-app.get('/hello',(req,res)=>{
-    res.send("Task Manager App");
-    res.end();
-})
-app.use('/api/v1/tasks',tasks);
-
-//app.get('/api/v1/tasks')        -get all tasks
-//app.get('/api/v1/tasks/:id')    -get a task
-//app.post('/api/v1/tasks')       -create a task
-//app.patch('/api/v1/tasks/:id')  -update a task
-//app.delete('/api/v1/tasks/:id') -delete a task
